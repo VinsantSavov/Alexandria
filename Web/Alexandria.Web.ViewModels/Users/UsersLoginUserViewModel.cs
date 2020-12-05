eaf6241ee7cf309a33c4e8b8a242ae891747067b
VinsantSavov <vinsantsvetoslavovsavov@gmail.com>
2020-12-05 21:02:37 +0200

    Creating Login view component

---
+++ Web/Alexandria.Web.ViewModels/Users/UsersLoginUserViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Alexandria.Web.ViewModels.Users
+{
+    using Alexandria.Data.Models;
+    using Alexandria.Services.Mapping;
+
+    public class UsersLoginUserViewModel : IMapFrom<ApplicationUser>
+    {
+        public string Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string ProfilePicture { get; set; }
+    }
+}
---
+++ Web/Alexandria.Web/Components/LoginUserViewComponent.cs
@@ -0,0 +1,29 @@
+﻿namespace Alexandria.Web.Components
+{
+    using System.Security.Claims;
+    using System.Threading.Tasks;
+
+    using Alexandria.Services.Users;
+    using Alexandria.Web.Infrastructure.Extensions;
+    using Alexandria.Web.ViewModels.Users;
+    using Microsoft.AspNetCore.Mvc;
+
+    [ViewComponent(Name = "LoginUser")]
+    public class LoginUserViewComponent : ViewComponent
+    {
+        private readonly IUsersService usersService;
+
+        public LoginUserViewComponent(IUsersService usersService)
+        {
+            this.usersService = usersService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(ClaimsPrincipal claimsPrincipal)
+        {
+            var userId = claimsPrincipal.GetUserId();
+            var user = await this.usersService.GetUserByIdAsync<UsersLoginUserViewModel>(userId);
+
+            return this.View(user);
+        }
+    }
+}
