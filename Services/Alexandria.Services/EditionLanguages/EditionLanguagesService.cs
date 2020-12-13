1c690643b26b6883a8d4d0ca4f9dbd4943c262fc
VinsantSavov <vinsantsvetoslavovsavov@gmail.com>
2020-12-13 16:38:23 +0200

    Testing edition languages service

--- Services/Alexandria.Services/EditionLanguages/EditionLanguagesService.cs
+++ Services/Alexandria.Services/EditionLanguages/EditionLanguagesService.cs
@@ -5,7 +5,6 @@
     using System.Threading.Tasks;
 
     using Alexandria.Data;
-    using Alexandria.Data.Models;
     using Alexandria.Services.Mapping;
     using Microsoft.EntityFrameworkCore;
 
@@ -16,17 +15,6 @@
         public EditionLanguagesService(AlexandriaDbContext db)
         {
             this.db = db;
-        }
-
-        public async Task CreateEditionLanguageAsync(string name)
-        {
-            var language = new EditionLanguage
-            {
-                Name = name,
-            };
-
-            await this.db.EditionLanguages.AddAsync(language);
-            await this.db.SaveChangesAsync();
         }
 
         public async Task<bool> DoesEditionLanguageIdExistAsync(int id)
@@ -40,15 +28,6 @@
                                                           .ToListAsync();
 
             return languages;
-        }
-
-        public async Task<TModel> GetEditionLanguageByIdAsync<TModel>(int id)
-        {
-            var language = await this.db.EditionLanguages.Where(l => l.Id == id)
-                                                   .To<TModel>()
-                                                   .FirstOrDefaultAsync();
-
-            return language;
         }
     }
 }
--- Services/Alexandria.Services/EditionLanguages/IEditionLanguagesService.cs
+++ Services/Alexandria.Services/EditionLanguages/IEditionLanguagesService.cs
@@ -5,12 +5,8 @@
 
     public interface IEditionLanguagesService
     {
-        Task CreateEditionLanguageAsync(string name);
-
         Task<bool> DoesEditionLanguageIdExistAsync(int id);
 
         Task<IEnumerable<TModel>> GetAllLanguagesAsync<TModel>();
-
-        Task<TModel> GetEditionLanguageByIdAsync<TModel>(int id);
     }
 }
